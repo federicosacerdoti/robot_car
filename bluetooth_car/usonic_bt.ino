b5dccc6d0df805c671695d2aab871042c52d0275
Federico Sacerdoti <fsacerdoti@aientech.com>
2019-08-31 13:32:04 +0100

    formatting

--- bluetooth_car/usonic_bt.ino
+++ bluetooth_car/usonic_bt.ino
@@ -90,101 +90,101 @@ void _mStop()
 void ultrasonicCheck()
 {
   digitalWrite(LED_BUILTIN, HIGH);   
-      delay(500);                      
-      digitalWrite(LED_BUILTIN, LOW);    
-      delay(500);
-          
-      //Move back a tiny bit
-      _mStop();
-      delay(300);                         
-      _mBack();
-      delay(150);
-      _mStop();
-      delay(500);
-      // Take right measurement                     
-      myservo.write(30);          
-      delay(1000);      
-      rightDistance = Distance_test();
+  delay(500);                      
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(500);
+      
+  //Move back a tiny bit
+  _mStop();
+  delay(300);                         
+  _mBack();
+  delay(150);
+  _mStop();
+  delay(500);
+  // Take right measurement                     
+  myservo.write(30);          
+  delay(1000);      
+  rightDistance = Distance_test();
 
-      #ifdef send
-      Serial.print("rightDistance=");
-      Serial.println(rightDistance);
-      #endif
+  #ifdef send
+  Serial.print("rightDistance=");
+  Serial.println(rightDistance);
+  #endif
 
-      // Take left measurement
-      delay(500);
-      myservo.write(90);      
-      delay(1000);                                                  
-      myservo.write(180);              
-      delay(1000); 
-      leftDistance = Distance_test(); 
+  // Take left measurement
+  delay(500);
+  myservo.write(90);      
+  delay(1000);                                                  
+  myservo.write(180);              
+  delay(1000); 
+  leftDistance = Distance_test(); 
 
-      #ifdef send
-      Serial.print("leftDistance=");
-      Serial.println(leftDistance);
-      #endif
+  #ifdef send
+  Serial.print("leftDistance=");
+  Serial.println(leftDistance);
+  #endif
 
-      //Re-center ultrasonic sensor
-      delay(300);
-      myservo.write(90);              
-      delay(300);
+  //Re-center ultrasonic sensor
+  delay(300);
+  myservo.write(90);              
+  delay(300);
 
 
-      if(rightDistance>leftDistance)  
-      {
-        _mright();
-        delay(100);
-        middlecheck();
-        delay(1500);
-       }
-       else if(rightDistance<leftDistance)
-       {
-        _mleft();
-        delay(100);
-        middlecheck();
-        delay(1500);
-       }
-       else if((rightDistance<=30)||(leftDistance<=30))
-       {
-        _mBack();
-        delay(500);
-       }
-       else if (middleDistance<=30) {
-        _mForward();
-        delay(200);
-        middlecheck(); 
-      }
-       else
-       {
-        _mForward();
-        delay(100);
-        middlecheck();
-       }
-    }  
+  if(rightDistance>leftDistance)  
+  {
+    _mright();
+    delay(100);
+    middlecheck();
+    delay(1500);
+   }
+   else if(rightDistance<leftDistance)
+   {
+    _mleft();
+    delay(100);
+    middlecheck();
+    delay(1500);
+   }
+   else if((rightDistance<=30)||(leftDistance<=30))
+   {
+    _mBack();
+    delay(500);
+   }
+   else if (middleDistance<=30) {
+    _mForward();
+    delay(200);
+    middlecheck(); 
+  }
+   else
+   {
+    _mForward();
+    delay(100);
+    middlecheck();
+   }
+}  
 
 void middlecheck()
 {
   myservo.write(90);
   middleDistance = Distance_test();
-      //Just quickly check that we aren't driving straight into something
-      if(middleDistance<=30)
-      {
-      ultrasonicCheck();
-      } else {
-        if (getstr == 'g') {
-        _mForward();
-      }
+  //Just quickly check that we aren't driving straight into something
+  if(middleDistance<=30)
+  {
+    ultrasonicCheck();
+  } else {
+    if (getstr == 'g') {
+    _mForward();
+    }
   }
 }
 
 void checkbtinput() 
 {
   getstr = Serial.read();
-    if(getstr == 'g'){
-      _mForward();
-    } else if (getstr == 's') {
-      _mStop();
-    }
+  if(getstr == 'g'){
+    _mForward();
+  } else if (getstr == 's') {
+    _mStop();
+  }
 }
 
 void setup() 
@@ -215,44 +215,42 @@ void loop()
     Serial.print("middleDistance=");
     Serial.println(middleDistance);
     #endif
-      // Take right measurement                     
-      myservo.write(30);          
-      delay(500);      
-      rightDistance = Distance_test();
-      if (rightDistance<=15) 
-      {
-        ultrasonicCheck();
-      }
-      checkbtinput();
-      #ifdef send
-      Serial.print("rightDistance=");
-      Serial.println(rightDistance);
-      #endif
+    // Take right measurement                     
+    myservo.write(30);          
+    delay(500);      
+    rightDistance = Distance_test();
+    if (rightDistance<=15) 
+    {
+      ultrasonicCheck();
+    }
+    checkbtinput();
+    #ifdef send
+    Serial.print("rightDistance=");
+    Serial.println(rightDistance);
+    #endif
 
-      // Take left measurement
-      delay(100);
-      myservo.write(90);   
-      delay(100);
-      middlecheck();
-      delay(100);                                                  
-      myservo.write(180);              
-      delay(100); 
-      checkbtinput();
-      leftDistance = Distance_test(); 
-      if (leftDistance<=15)
-      {
-        ultrasonicCheck();
-      } 
-      #ifdef send
-      Serial.print("leftDistance=");
-      Serial.println(leftDistance);
-      #endif
+    // Take left measurement
+    delay(100);
+    myservo.write(90);   
+    delay(100);
+    middlecheck();
+    delay(100);                                                  
+    myservo.write(180);              
+    delay(100); 
+    checkbtinput();
+    leftDistance = Distance_test(); 
+    if (leftDistance<=15)
+    {
+      ultrasonicCheck();
+    } 
+    #ifdef send
+    Serial.print("leftDistance=");
+    Serial.println(leftDistance);
+    #endif
 
-      //Re-center ultrasonic sensor
-      delay(300);
-      myservo.write(90);              
-      middlecheck();
-      delay(200);
-   
+    //Re-center ultrasonic sensor
+    delay(300);
+    myservo.write(90);              
+    middlecheck();
+    delay(200);
 }
-
